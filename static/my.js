3aa61bec9a32fd62378baf05824b2078b674bf05
wth-code <wongsstudio@gmail.com>
2021-01-15 10:13:12 +0800

    update

---
+++ static/my.js
@@ -0,0 +1,6 @@
+$("button").click(function() {
+    var $btn = $(this);
+    $btn.button('loading');
+    // Then whatever you actually want to do i.e. submit form
+    // After that has finished, reset the button state using
+});
