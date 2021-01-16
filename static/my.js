ec1474fb4bd6700c128fc1e171c80460a9c6fd60
Tim <wongsstudio@gmail.com>
2021-01-16 15:19:22 +0800

    update

--- static/my.js
+++
@@ -1,6 +0,0 @@
-$("button").click(function() {
-    var $btn = $(this);
-    $btn.button('loading');
-    // Then whatever you actually want to do i.e. submit form
-    // After that has finished, reset the button state using
-});
