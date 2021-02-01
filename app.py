4f75ecfc6bd03f2b4a6faf212a95326d62356fbe
Tim <wongsstudio@gmail.com>
2021-02-01 18:00:18 +0800

    update

--- app.py
+++ app.py
@@ -241,21 +241,11 @@ def p():
 
 @app.route("/stock")
 def stock():
-    return get_items(get_date_now()) + "other dates please go to link https://wth-code-emailsender-web.zeet.app/stock/(date) date format is dd-mm-yy"
+    return get_items(get_date_now())
 
 @app.route("/stock/<date>")
 def stock_date(date):
     return get_items(date)
-
-# ========================= END STOCK =============================
-
-class ItemTable(Table):
-    name = Col('Time')
-    description = Col('Price')
-
-def get_date_now():
-    country_time = datetime.now(country_time_zone)
-    return country_time.strftime("%d-%m-%y")
 
 def get_items(date):
     result = firebase.get(date, "")
@@ -264,6 +254,18 @@ def get_items(date):
         items.append(dict(name=time, description=price))
     table = ItemTable(items, border="1")
     return table.__html__()
+
+class ItemTable(Table):
+    name = Col('Time')
+    description = Col('Price')
+
+
+# ========================= END STOCK =============================
+
+
+def get_date_now():
+    country_time = datetime.now(country_time_zone)
+    return country_time.strftime("%d-%m-%y")
 
 def listToString(s):
     # initialize an empty string
